9a341f9c4ff72271f179e3778d11cafdc75f5511
Gonzales <grober_gonzales@hotmail.com>
2021-11-10 08:21:39 -0500

    modify 3

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 
+void sayHello(std::string name){
+	std::cout << "Hello " << name << std::endl;
+}
+
 int main(int args, const char **kargs){
 	std::cout << "Hello World!" << std::endl;
 	std::cout << "New Change" << std::endl;
 	std::cout << "New Change ToMorrow" << std::endl;
+
+	sayHello("Poggers");
 }
