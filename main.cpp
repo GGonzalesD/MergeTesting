5628b22565d37d7a2798eef5fc26f7fbad9fdea9
Gonzales <grober_gonzales@hotmail.com>
2021-11-10 08:16:11 -0500

    Add Msg

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 
+void function(std::string message){
+	std::cout << "Msg: " << message << std::endl;
+}
+
 int main(int args, const char **kargs){
 	std::cout << "Hello World!" << std::endl;
 	std::cout << "New Change" << std::endl;
 	std::cout << "New Change Today" << std::endl;
+
+	function("Hola");
 }
